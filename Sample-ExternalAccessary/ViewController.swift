93107a04a9e04185bbe529854f84b1db6d7e0956
takumi314 <isospin_symmetry@me.com>
2017-12-23 20:04:58 +0900

    first commit

---
+++ Sample-ExternalAccessary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample-ExternalAccessary
+//
+//  Created by NishiokaKohei on 2017/12/23.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
