488e791581ab0cc3b33e5e8f167a9475369b59ff
takumi314 <isospin_symmetry@me.com>
2017-12-24 00:45:15 +0900

    make new file

---
+++ Sample-ExternalAccessary/ExternalAccessoryMediator.swift
@@ -0,0 +1,18 @@
+//
+//  ExternalAccessoryMediator.swift
+//  Sample-ExternalAccessary
+//
+//  Created by NishiokaKohei on 2017/12/24.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
+import ExternalAccessory
+
+protocol EAMediating {
+
+}
+
+class ExternalAccessoryMediator: NSObject {
+
+}
