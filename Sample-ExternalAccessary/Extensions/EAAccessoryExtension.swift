85bbc264746794c847e83a16c78217fa879fa2ab
takumi314 <isospin_symmetry@me.com>
2017-12-26 03:46:47 +0900

    implement determinant method if a given protocol name is same as connected accessory’s

--- Sample-ExternalAccessary/Extensions/EAAccessoryExtension.swift
+++ Sample-ExternalAccessary/Extensions/EAAccessoryExtension.swift
@@ -11,4 +11,19 @@ import ExternalAccessory
 
 extension EAAccessory {
 
+    ///
+    /// @protocolName 外部接続先のプロトコルが含まれるかを判定する関数を指定する.
+    /// @return 判定結果がBool型で返される。
+    ///
+    func accessible(with protocolName: @escaping (String) -> Bool) -> Bool {
+        return protocolStrings.contains(where: protocolName)
+    }
+
+    ///
+    /// 接続中の外部接続先で, 指定したプロトコルを含むものがあるか判定する.
+    ///
+    func contains(protocol name: String) -> Bool {
+        return protocolStrings.contains(name)
+    }
+
 }
