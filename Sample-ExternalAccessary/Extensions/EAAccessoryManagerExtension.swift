e4df91fb444aabd2946b41c58443e5ca657540e7
takumi314 <isospin_symmetry@me.com>
2017-12-27 03:36:50 +0900

    create new file for class extension

---
+++ Sample-ExternalAccessary/Extensions/EAAccessoryManagerExtension.swift
@@ -0,0 +1,9 @@
+//
+//  EAAccessoryManagerExtension.swift
+//  Sample-ExternalAccessary
+//
+//  Created by NishiokaKohei on 2017/12/27.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
