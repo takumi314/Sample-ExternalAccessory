00103c9d6a642ec582c7be9291c554ccfbae59cb
takumi314 <isospin_symmetry@me.com>
2017-12-28 13:25:04 +0900

    add new file for extension

---
+++ Sample-ExternalAccessory/Extensions/EASessionExtension.swift
@@ -0,0 +1,14 @@
+//
+//  EASessionExtension.swift
+//  Sample-ExternalAccessory
+//
+//  Created by NishiokaKohei on 2017/12/28.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
+import ExternalAccessory
+
+extension EASession {
+
+}
