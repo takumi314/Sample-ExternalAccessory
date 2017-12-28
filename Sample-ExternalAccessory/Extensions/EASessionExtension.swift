12fa4efc9c41b6701380e1f4b210986294a979c6
takumi314 <isospin_symmetry@me.com>
2017-12-28 19:09:07 +0900

    remove convenience initializer

--- Sample-ExternalAccessory/Extensions/EASessionExtension.swift
+++ Sample-ExternalAccessory/Extensions/EASessionExtension.swift
@@ -15,15 +15,10 @@ extension EASession {
     /// To provide a EASession instance which is with arguments of EAAccessing and ProtocolName.
     ///
     static func create(accessory: EAAccessing, forProtocol protocolString: ProtocolName) -> EASession? {
-        return EASession(accessory: accessory, forProtocol: protocolString)
-    }
-
-    convenience init?(accessory: EAAccessing, forProtocol protocolString: ProtocolName) {
-        if let accessory = accessory as? EAAccessory {
-            self.init(accessory: accessory, forProtocol: protocolString)
-        } else {
-            self.init(accessory: accessory as! EAAccessory, forProtocol: protocolString)
+        guard let accessory = accessory as? EAAccessory else {
+            return nil
         }
+        return EASession(accessory: accessory, forProtocol: protocolString)
     }
 
 }
