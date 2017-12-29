42695dc9740fbc2175c1ee48459a1d61a39a5942
takumi314 <isospin_symmetry@me.com>
2017-12-29 16:53:41 +0900

    add new file for wrapping EASession

---
+++ Sample-ExternalAccessory/EeternalAccessories/ExternalAccessoryDispatcher.swift
@@ -0,0 +1,21 @@
+//
+//  ExternalAccessoryDispatcher.swift
+//  Sample-ExternalAccessory
+//
+//  Created by NishiokaKohei on 2017/12/29.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
+
+protocol Dispatchable {
+
+}
+
+class ExternalAccessoryDispatcher: NSString {
+
+}
+
+extension ExternalAccessoryDispatcher {
+
+}
