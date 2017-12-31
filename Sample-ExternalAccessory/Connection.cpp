83c79627b3c33680f0f4f9cbc03ed571bbbcdf13
takumi314 <isospin_symmetry@me.com>
2017-12-31 23:53:21 +0900

    add cpp file for Socket stream

---
+++ Sample-ExternalAccessory/Connection.cpp
@@ -0,0 +1,9 @@
+//
+//  Connection.cpp
+//  Sample-ExternalAccessory
+//
+//  Created by NishiokaKohei on 2017/12/31.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+#include "Connection.hpp"
---
+++ Sample-ExternalAccessory/Connection.hpp
@@ -0,0 +1,14 @@
+//
+//  Connection.hpp
+//  Sample-ExternalAccessory
+//
+//  Created by NishiokaKohei on 2017/12/31.
+//  Copyright © 2017年 Kohey.Nishioka. All rights reserved.
+//
+
+#ifndef Connection_hpp
+#define Connection_hpp
+
+#include <stdio.h>
+
+#endif /* Connection_hpp */
