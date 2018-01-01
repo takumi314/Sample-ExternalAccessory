f127e94aa0fcf8609dada8e28d5be36e6ce7e7c3
takumi314 <isospin_symmetry@me.com>
2018-01-01 16:56:49 +0900

    add new file

---
+++ Sample-ExternalAccessory/EeternalAccessories/EADispatchable.swift
@@ -0,0 +1,9 @@
+//
+//  EADispatchable.swift
+//  Sample-ExternalAccessory
+//
+//  Created by NishiokaKohei on 2018/01/01.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
