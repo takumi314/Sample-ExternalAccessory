ffb21aeabbbf76b6bd1df1a1d485da5d42c841ec
takumi314 <isospin_symmetry@me.com>
2018-01-04 02:09:19 +0900

    add new ValueObject

---
+++ Sample-ExternalAccessory/EeternalAccessories/AccessoryInfo.swift
@@ -0,0 +1,13 @@
+//
+//  AccessoryInfo.swift
+//  Sample-ExternalAccessory
+//
+//  Created by NishiokaKohei on 2018/01/04.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
+
+struct AccessoryInfo {
+    
+}
