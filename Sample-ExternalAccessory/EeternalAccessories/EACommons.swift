fce8055dc5851130edd80d3c81b6d026ed6678d3
takumi314 <isospin_symmetry@me.com>
2018-01-05 02:14:04 +0900

    add new file for constant and common value

---
+++ Sample-ExternalAccessory/EeternalAccessories/EACommons.swift
@@ -0,0 +1,9 @@
+//
+//  EACommons.swift
+//  Sample-ExternalAccessory
+//
+//  Created by NishiokaKohei on 2018/01/05.
+//  Copyright © 2018年 Kohey.Nishioka. All rights reserved.
+//
+
+import Foundation
